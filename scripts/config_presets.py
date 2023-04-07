44231f2b3c674ebfc37956ebade4df58796050de
cjx <cjx@cjxdeMacBook-Pro.local>
2023-04-07 18:29:37 +0800

    debug

--- scripts/config_presets.py
+++ scripts/config_presets.py
@@ -57,7 +57,7 @@ class Script(scripts.Script):
             "ext_ctl_resize_mode",
             "ext_ctl_canvas_width",
             "ext_ctl_canvas_height",
-            # "ext_ctl_image",
+            "Invert Input Color",
            # "component-1285",
         ]
         self.img2img_component_ids = [   # mirrors the config_preset_dropdown.change(output) events and config_preset_dropdown_change()
@@ -143,11 +143,14 @@ class Script(scripts.Script):
     #     print("111111111111")
 
     def after_component(self, component, **kwargs):
-        print(component.elem_id)
-        print(component.label)
-        print("==========")
         # to generalize the code, detect if we are in txt2img tab or img2img tab, and then use the corresponding self variables
         # so we can use the same code for both tabs
+        if not hasattr(component, 'elem_id'):
+            component.elem_id=component.label
+
+        print(component.elem_id)
+        print(component.label)
+        print("========")
         component_map = None
         component_ids = None
         config_file_name = None
@@ -169,7 +172,6 @@ class Script(scripts.Script):
         #if component.elem_id == "txt2img_style2_index": #doesn't work, need to be added after all the components we edit are loaded
         #if component.elem_id == "open_folder": #bottom of the image gallery
         if component.elem_id == "txt2img_generation_info_button" or component.elem_id == "img2img_generation_info_button": #very bottom of the txt2img/img2img image gallery
-            print(component.elem_id)
             #print("Creating dropdown values...")
             #print("key/value pairs in component_map:")
             # before we create the dropdown, we need to check if each component was found successfully to prevent errors from bricking the Web UI
